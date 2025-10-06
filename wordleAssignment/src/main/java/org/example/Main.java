3d9b41e062338931e8fad18fa6e8ff9bcabd3214
G <gegigson.cpp.edu@gmail.com>
2025-10-05 17:48:16 -0700

    got rid of unneeded on main

--- wordleAssignment/src/main/java/org/example/Main.java
+++ wordleAssignment/src/main/java/org/example/Main.java
@@ -15,38 +15,5 @@ public class Main {
         });
         //Temporary early exit
         //System.exit(0);
-        Scanner scnr = new Scanner(System.in);
-        String solution = "Party";
-        //String guess;
-        System.out.println("Hello and welcome to our wordle game!");
-        //System.out.println("The correct word was: " + solution);
-    }
-
-    public static String inputValidation(Scanner scnr){
-        String userInput;
-        boolean notChar;
-        do{
-            notChar = false;
-            System.out.println("Enter your guess");
-            userInput = scnr.next();
-            //Checks if input is 5 chars
-            if(userInput.length() != 5){
-                System.out.println("Invalid Input! Enter a 5 digit word");
-            }
-
-            //Breaks 5 letter string down into characters and checks if they are from a-z
-            for(int i = 0; i < userInput.length(); i++){
-                //System.out.println(userInput.charAt(i));
-                if(!(Character.isLetter(userInput.charAt(i)))){
-                    notChar = true;
-                }
-            }
-            if(notChar){
-                System.out.println("Invalid input! Values must be from a-z");
-            }
-        }
-        while(userInput.length()!= 5  || (notChar));
-
-        return userInput;
     }
 }
