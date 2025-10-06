264d0f28d8dd146c83dee68d983866784cc7711b
G <gegigson.cpp.edu@gmail.com>
2025-10-05 17:45:42 -0700

    reformatted a little

--- wordleAssignment/src/main/java/org/example/MainWindow.java
+++ wordleAssignment/src/main/java/org/example/MainWindow.java
@@ -20,7 +20,7 @@ public class MainWindow {
     final String solution = "Party";
 
 
-    public MainWindow(){
+    public MainWindow() {
         frame = new JFrame();
         frame.setTitle("Wordle.exe");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -43,7 +43,7 @@ public class MainWindow {
         //Subject to optimization
         JPanel gTextPanel1 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel1.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[0][i] = new JLabel("___");
             gTextPanel1.add(w[0][i]);
         }
@@ -51,7 +51,7 @@ public class MainWindow {
 
         JPanel gTextPanel2 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel2.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[1][i] = new JLabel("___");
             gTextPanel2.add(w[1][i]);
         }
@@ -59,7 +59,7 @@ public class MainWindow {
 
         JPanel gTextPanel3 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel3.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[2][i] = new JLabel("___");
             gTextPanel3.add(w[2][i]);
         }
@@ -67,7 +67,7 @@ public class MainWindow {
 
         JPanel gTextPanel4 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel4.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[3][i] = new JLabel("___");
             gTextPanel4.add(w[3][i]);
         }
@@ -75,7 +75,7 @@ public class MainWindow {
 
         JPanel gTextPanel5 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel5.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[4][i] = new JLabel("___");
             gTextPanel5.add(w[4][i]);
         }
@@ -83,7 +83,7 @@ public class MainWindow {
 
         JPanel gTextPanel6 = new JPanel(new FlowLayout(FlowLayout.CENTER, 8, 0));
         gTextPanel6.setLayout(new FlowLayout(FlowLayout.CENTER));
-        for(int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             w[5][i] = new JLabel("___");
             gTextPanel6.add(w[5][i]);
         }
@@ -97,22 +97,22 @@ public class MainWindow {
         //Also calls keyInputValidation to prevent digits and non alphabet chars from being called
         //Below I started on some code making gchar an array instead, but reverted to original(DELETE BEFORE SUBMISSION)
         /**
-        gChar = new JTextField[6];
-        for(int i = 0; i < 5; i++){
-            //JTextField gChar[i] = new JTextField(1);
-            gChar[i].setFont(new Font("Arial", Font.BOLD, 22));
-            gChar[i].setHorizontalAlignment(JTextField.CENTER);
-            gChar[i].setPreferredSize(new Dimension(40,40));
-            textPanel.add(gChar[i]);
-            gChar[i].addKeyListener(new keyInputValidation(gChar[i]));
-        }
+         gChar = new JTextField[6];
+         for(int i = 0; i < 5; i++){
+         //JTextField gChar[i] = new JTextField(1);
+         gChar[i].setFont(new Font("Arial", Font.BOLD, 22));
+         gChar[i].setHorizontalAlignment(JTextField.CENTER);
+         gChar[i].setPreferredSize(new Dimension(40,40));
+         textPanel.add(gChar[i]);
+         gChar[i].addKeyListener(new keyInputValidation(gChar[i]));
+         }
          **/
 
 
         JTextField gChar1 = new JTextField(1);
         gChar1.setFont(new Font("Arial", Font.BOLD, 22));
         gChar1.setHorizontalAlignment(JTextField.CENTER);
-        gChar1.setPreferredSize(new Dimension(40,40));
+        gChar1.setPreferredSize(new Dimension(40, 40));
         textPanel.add(gChar1);
         gChar1.addKeyListener(new keyInputValidation(gChar1));
 
@@ -120,31 +120,30 @@ public class MainWindow {
         JTextField gChar2 = new JTextField(1);
         gChar2.setFont(new Font("Arial", Font.BOLD, 22));
         gChar2.setHorizontalAlignment(JTextField.CENTER);
-        gChar2.setPreferredSize(new Dimension(40,40));
+        gChar2.setPreferredSize(new Dimension(40, 40));
         textPanel.add(gChar2);
         gChar2.addKeyListener(new keyInputValidation(gChar2));
 
         JTextField gChar3 = new JTextField(1);
         gChar3.setFont(new Font("Arial", Font.BOLD, 22));
         gChar3.setHorizontalAlignment(JTextField.CENTER);
-        gChar3.setPreferredSize(new Dimension(40,40));
+        gChar3.setPreferredSize(new Dimension(40, 40));
         textPanel.add(gChar3);
         gChar3.addKeyListener(new keyInputValidation(gChar3));
 
         JTextField gChar4 = new JTextField(1);
         gChar4.setFont(new Font("Arial", Font.BOLD, 22));
         gChar4.setHorizontalAlignment(JTextField.CENTER);
-        gChar4.setPreferredSize(new Dimension(40,40));
+        gChar4.setPreferredSize(new Dimension(40, 40));
         textPanel.add(gChar4);
         gChar4.addKeyListener(new keyInputValidation(gChar4));
 
         JTextField gChar5 = new JTextField(1);
         gChar5.setFont(new Font("Arial", Font.BOLD, 22));
         gChar5.setHorizontalAlignment(JTextField.CENTER);
-        gChar5.setPreferredSize(new Dimension(40,40));
+        gChar5.setPreferredSize(new Dimension(40, 40));
         textPanel.add(gChar5);
         gChar5.addKeyListener(new keyInputValidation(gChar5));
-
 
 
         JButton enterButton = new JButton("Submit guess");
@@ -153,17 +152,17 @@ public class MainWindow {
             @Override
             public void actionPerformed(ActionEvent e) {
                 /**
-                for(int i = 0; i < 5; i++){
-                    for(int j = 0; j < 5; j++){
-                        w[i][j].setSize(new Dimension(40, 40));
-                        w[i][j].setFont(new Font("Arial", Font.BOLD, 22));
-                    }
-                }
+                 for(int i = 0; i < 5; i++){
+                 for(int j = 0; j < 5; j++){
+                 w[i][j].setSize(new Dimension(40, 40));
+                 w[i][j].setFont(new Font("Arial", Font.BOLD, 22));
+                 }
+                 }
                  **/
                 //solution = solution.toUpperCase();
                 int[] colorLayout = new int[5];
                 String[] guess = {gChar1.getText(), gChar2.getText(), gChar3.getText(), gChar4.getText(), gChar5.getText()};
-                for(int i = 0; i < solution.length(); i++){
+                for (int i = 0; i < solution.length(); i++) {
                     //each initializes the string version of the solution char
                     //compares it to the input char
                     //initializes guess character
@@ -171,22 +170,22 @@ public class MainWindow {
                     //initializes solution char
                     char soluC = Character.toUpperCase(solution.charAt(i));
                     //if they equal each other print set color to green
-                    if(guessC == soluC){
+                    if (guessC == soluC) {
                         //System.out.println("Match");
                         colorLayout[i] = 1;
                     }
-                    for(int j = 0; j < solution.length(); j++){
+                    for (int j = 0; j < solution.length(); j++) {
                         //exits loop if already green
-                        if(colorLayout[i] == 1){
-                        break;
+                        if (colorLayout[i] == 1) {
+                            break;
                         }
-                        if((solution.toUpperCase().contains(String.valueOf(guessC)))){
+                        if ((solution.toUpperCase().contains(String.valueOf(guessC)))) {
                             colorLayout[i] = 2;
                         }
                     }
                 }
 
-                for(int i = 0; i < 5; i++) {
+                for (int i = 0; i < 5; i++) {
                     System.out.println(colorLayout[i]);
                 }
                 String gWord = gChar1.getText() + gChar2.getText() + gChar3.getText() + gChar4.getText() + gChar5.getText();
@@ -195,7 +194,8 @@ public class MainWindow {
 
                 //Want to make it so that after it checks if the word is an actual dictionary word it  continues and updates the guess num
                 //Switch statement below takes the guess num and outputs it with the guess string(with designated colors)
-                switch(guessNum){
+                //could be made into a for or something to make code more compact
+                switch (guessNum) {
                     case 1:
 
                         //w[0][0].setText(gChar[0].getText());
@@ -203,11 +203,11 @@ public class MainWindow {
                         //w[0][3].setForeground(Color.GREEN);
                         //w1.setForeground(Color.GREEN);
                         //Code below sets the characters to the users guess(green implementation)
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
                             } else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(new Color(225,225,0));
+                                w[guessNum - 1][i].setForeground(new Color(225, 225, 0));
                             }
                         }
                         //redundant code, can be optimized
@@ -224,12 +224,11 @@ public class MainWindow {
 
                         break;
                     case 2:
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
-                            }
-                            else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(Color.YELLOW);
+                            } else if (colorLayout[i] == 2) {
+                                w[guessNum - 1][i].setForeground(Color.YELLOW);
                             }
                         }
                         w[guessNum - 1][0].setText(gChar1.getText());
@@ -244,12 +243,11 @@ public class MainWindow {
                         w[guessNum - 1][4].setFont(new Font("Arial", Font.BOLD, 32));
                         break;
                     case 3:
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
-                            }
-                            else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(Color.YELLOW);
+                            } else if (colorLayout[i] == 2) {
+                                w[guessNum - 1][i].setForeground(Color.YELLOW);
                             }
                         }
                         w[guessNum - 1][0].setText(gChar1.getText());
@@ -264,12 +262,11 @@ public class MainWindow {
                         w[guessNum - 1][4].setFont(new Font("Arial", Font.BOLD, 32));
                         break;
                     case 4:
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
-                            }
-                            else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(Color.YELLOW);
+                            } else if (colorLayout[i] == 2) {
+                                w[guessNum - 1][i].setForeground(Color.YELLOW);
                             }
                         }
                         w[guessNum - 1][0].setText(gChar1.getText());
@@ -284,12 +281,11 @@ public class MainWindow {
                         w[guessNum - 1][4].setFont(new Font("Arial", Font.BOLD, 32));
                         break;
                     case 5:
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
-                            }
-                            else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(Color.YELLOW);
+                            } else if (colorLayout[i] == 2) {
+                                w[guessNum - 1][i].setForeground(Color.YELLOW);
                             }
                         }
                         w[guessNum - 1][0].setText(gChar1.getText());
@@ -304,12 +300,11 @@ public class MainWindow {
                         w[guessNum - 1][4].setFont(new Font("Arial", Font.BOLD, 32));
                         break;
                     case 6:
-                        for(int i = 0; i < 5; i++){
-                            if(colorLayout[i] == 1){
+                        for (int i = 0; i < 5; i++) {
+                            if (colorLayout[i] == 1) {
                                 w[guessNum - 1][i].setForeground(Color.GREEN);
-                            }
-                            else if (colorLayout[i] == 2) {
-                                w[guessNum-1][i].setForeground(Color.YELLOW);
+                            } else if (colorLayout[i] == 2) {
+                                w[guessNum - 1][i].setForeground(Color.YELLOW);
                             }
                         }
                         w[guessNum - 1][0].setText(gChar1.getText());
@@ -337,11 +332,12 @@ public class MainWindow {
         panel.add(enterButton);
     }
 
-    public void show(){
+    public void show() {
         frame.setVisible(true);
     }
+
     //checks if character entered is a letter with regex
-    private static boolean validInput(String input){
+    private static boolean validInput(String input) {
         return input.matches("[A-Za-z]");
     }
 }
